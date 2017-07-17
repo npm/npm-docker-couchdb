26e4e625f45cf79a519e878814852fe0475d2291
Benjamin Coe <ben@npmjs.com>
2017-07-17 14:51:01 -0700

    feat: create filenamedb database [build-image]

--- install-couch-app.sh
+++ install-couch-app.sh
@@ -8,7 +8,11 @@ until $(curl --output /dev/null --silent --head --fail http://localhost:5984/); 
     sleep 2
 done
 
+# Create database for registry.
 curl -XPUT http://admin:admin@localhost:5984/registry
+# Create database for store-file.
+curl -XPUT http://admin:admin@localhost:5984/filenamedb
+
 DEPLOY_VERSION=testing npm start --npm-registry-couchapp:couch=http://admin:admin@localhost:5984/registry
 npm run load --npm-registry-couchapp:couch=http://admin:admin@localhost:5984/registry
 NO_PROMPT=true npm run copy --npm-registry-couchapp:couch=http://admin:admin@localhost:5984/registry
